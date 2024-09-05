3d1412a6a118f800cef61896763d36764da90312
ruan-narici <ruannarici@hotmail.com>
2024-09-04 23:12:02 -0300

    feat: ADD Music object

---
+++ src/main/java/br/com/ruannarici/consolemusic/model/Music.java
@@ -0,0 +1,32 @@
+package br.com.ruannarici.consolemusic.model;
+
+public class Music {
+
+    private Long id;
+    private String name;
+    private Artist artist;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+}
