9a99296e106cad207f3338d164c8f07f6e67c03a
ruan-narici <ruannarici@hotmail.com>
2024-09-04 23:02:52 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/ruannarici/consolemusic/ConsolemusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.ruannarici.consolemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolemusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolemusicApplication.class, args);
+	}
+
+}
