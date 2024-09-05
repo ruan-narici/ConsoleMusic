0c771c0cc6ecbaff7d715c12b652917899ac7e2c
ruan-narici <ruannarici@hotmail.com>
2024-09-04 23:21:31 -0300

    feat: ADD showMenu method in object Main

--- src/main/java/br/com/ruannarici/consolemusic/main/Main.java
+++ src/main/java/br/com/ruannarici/consolemusic/main/Main.java
@@ -1,8 +1,38 @@
 package br.com.ruannarici.consolemusic.main;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public void run() {
+    private Scanner reader = new Scanner(System.in);
 
+    public void run() {
+        showMenu();
+        Integer option = reader.nextInt();
+        reader.nextLine();
+        switch (option) {
+            case 1: {
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+        }
     }
+
+    private void showMenu() {
+        System.out.println("WELCOME TO THE *CONSOLE MUSIC*");
+        System.out.println("[MENU]");
+        System.out.println("1 - REGISTER NEW ARTIST");
+        System.out.println("2 - REGISTER NEW MUSIC");
+        System.out.println("3 - LIST ARTISTS");
+        System.out.println("4 - LIST MUSICS");
+    }
+
 }
