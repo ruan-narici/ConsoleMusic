1abed3d21fdf7ba4e9945f7592ab925cfb7ce4f3
ruan-narici <ruannarici@hotmail.com>
2024-09-04 23:14:12 -0300

    feat: ADD methods for Artist

--- src/main/java/br/com/ruannarici/consolemusic/model/Artist.java
+++ src/main/java/br/com/ruannarici/consolemusic/model/Artist.java
@@ -1,8 +1,44 @@
 package br.com.ruannarici.consolemusic.model;
 
+import java.util.List;
+
 public class Artist {
 
     private Long id;
     private String name;
+    private ECategory category;
+    private List<Music> musicList;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ECategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ECategory category) {
+        this.category = category;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void addMusic(Music music) {
+        music.setArtist(this);
+        this.musicList.add(music);
+    }
 }
